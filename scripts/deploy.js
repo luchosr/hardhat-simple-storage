598155f24cbe4dc1b410cdf4c0e7941ea5693d11
Luciano <luciano.ramello@ext.soprabanking.com>
2022-10-14 19:38:34 +0200

    update deploy

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,13 +1,22 @@
 // imports
 
+const { ethers } = require("hardhat")
+
 // async main
-async function main() {}
+async function main() {
+    const SimpleStorageFactory = await ethers.getContractFactory(
+        "SimppleStorage"
+    )
+    console.log("Deploying contract...")
+    const simpleStorage = await SimpleStorageFactory.deploy()
+    await simpleStorage.deployed()
+}
 
 // main
 
 main()
-  .then(() => process.exit(0))
-  .catch((error) => {
-    console.error(error);
-    process.exit(1);
-  });
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error)
+        process.exit(1)
+    })
